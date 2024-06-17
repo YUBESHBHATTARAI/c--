f32bc5e7ae8911f32a19a3dcd2b784ea3b964889
yubeshbhattarai <yubeshbhattarai@gmail.com>
2024-06-17 07:47:44 +0545

    ..

---
+++ addition.cpp
@@ -0,0 +1,12 @@
+//c++ program to add two numbers
+#include <iostream>
+int main() {
+    int num1, num2;
+    std::cout << "Enter the first number: ";
+    std::cin >> num1;
+    std::cout << "Enter the second number: ";
+    std::cin >> num2;
+    int sum = num1 + num2;
+    std::cout << "The sum is: " << sum << std::endl;
+    return 0;
+}
