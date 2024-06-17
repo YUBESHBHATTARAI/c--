695c9560233ab37c8acbeac3037ed7ce5470bba4
yubeshbhattarai <yubeshbhattarai@gmail.com>
2024-06-17 22:43:56 +0545

    swapping

---
+++ swapping.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+void swap(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int main() {
+    int num1 = 10;
+    int num2 = 20;
+
+    std::cout << "Before swapping: num1 = " << num1 << ", num2 = " << num2 << std::endl;
+
+    swap(num1, num2);
+
+    std::cout << "After swapping: num1 = " << num1 << ", num2 = " << num2 << std::endl;
+
+    return 0;
+}
